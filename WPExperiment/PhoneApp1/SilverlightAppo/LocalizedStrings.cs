9eea62a5ad4b320a4f442bdf88a8a40cb1ba53d0
panyibin <panyibin@outlook.com>
2014-08-29 17:31:30 +0800

    add a test wp silverlight app

---
+++ WPExperiment/PhoneApp1/SilverlightAppo/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using SilverlightAppo.Resources;
+
+namespace SilverlightAppo
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
